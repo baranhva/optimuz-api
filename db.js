39c309ccd0e07333cdc6fe995c7b6b6b4415c0df
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-17 13:05:37 +0100

    Create dockerfile for the api server and a docker-compose file that sets up the MySQL database & Redis server

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ module.exports = function(Sequelize) {
 
     let db = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        dialect: process.env.DB_DIALECT
     });
 
     return db;
