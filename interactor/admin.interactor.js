a4df72f8517db80ae1e00249a054d8b941584cfe
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-23 15:11:03 +0100

    Create admin interactor and admin routes and load them and the admin controller in server.js

---
+++ interactor/admin.interactor.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = function(UserService) {
+
+    let interactor = {};
+
+    interactor.getAllUsers = async function() {
+        return UserService.getAllUsers();
+    };
+
+    return ctrl;
+};
