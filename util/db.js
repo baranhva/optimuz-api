77d6e5b14c44d096a58240fafd700ba03f66891b
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-26 14:55:58 +0100

    Make all table names the same as the name of the model

--- util/db.js
+++ util/db.js
@@ -3,6 +3,9 @@
 module.exports = function(Sequelize) {
     return new Sequelize(process.env.DB_SCHEMA, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
         dialect: 'mysql',
-        host: process.env.DATABASE_HOST
+        host: process.env.DATABASE_HOST,
+        define: {
+            freezeTableName: true
+        }
     });
 };
