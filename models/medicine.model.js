58967ec207be6973fdd65b209b915a121db78ecb
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-26 14:55:43 +0100

    Create medicine model and the many to many relationship model user-medicine and link them together

---
+++ models/medicine.model.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = function(db, DataTypes) {
+
+    return db.define('Medicine', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        amountOfUnit: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        unit: {
+            type: DataTypes.ENUM(['Kg', 'g', 'mg', 'mcg', ' L', 'ml', 'cc', 'mol', 'mmol']),
+            allowNull: false,
+        },
+        amountPerPackage: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    });
+}
---
+++ models/user-medicine.model.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = function(db, DataTypes, User, Medicine) {
+
+    const UserMedicine = db.define('UserMedicine', {});
+
+    User.belongsToMany(Medicine, { through: UserMedicine });
+    Medicine.belongsToMany(User, { through: UserMedicine });
+
+    return UserMedicine;
+}
