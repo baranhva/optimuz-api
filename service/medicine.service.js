d9977f35831494b32ca6e3718fb33cdb9649c7d6
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-26 14:56:27 +0100

    Create medicine service which has function for creating medicine as a user and that directly links that medicine to that user

---
+++ service/medicine.service.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+module.exports = function(db, Medicine, UserMedicine) {
+
+    async function createMedicineAndLinkToPatient(userId, name, amountOfUnit, unit, amountPerPackage) {
+        try {
+            const transaction = await db.transaction();
+            const medicine = await createMedicine(name, amountOfUnit, unit, amountPerPackage, transaction);
+            await linkMedicineToUser(medicine.id.toString(), userId, transaction);
+            await transaction.commit();
+            return medicine.toJSON();
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
+    }
+
+    async function createMedicine(name, amountOfUnit, unit, amountPerPackage, transaction) {
+        return Medicine.create({
+            name: name, amountOfUnit: amountOfUnit, unit: unit, amountPerPackage: amountPerPackage
+        }, {transaction});
+    }
+
+    async function linkMedicineToUser(medicineId, userId, transaction) {
+        return UserMedicine.create({
+            medicine: medicineId,
+            user: userId
+        }, { transaction: transaction });
+    }
+
+}
